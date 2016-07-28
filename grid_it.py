3443640748c4e718203f8fe4062baf1c002df33b
Jean-Christophe Perrin <jc.m.perrin@gmail.com>
2016-07-28 21:26:48 +0530

    Created Python Script
    Wrote initial python script with broad overview of expected structure.
    This is a very linear program so expect few changes. Included logging
    when hits every main method.

---
+++ grid_it.py
@@ -0,0 +1,34 @@
+#!usr/bin/python
+
+#import modules
+from PIL import Image #library for manipulating imgs
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+def get_filename():
+    logging.debug('getting file')
+    return "filename.txt"
+
+def get_img(img):
+    logging.debug('getting img')
+    return "img.png"
+
+def change_img(img):
+    logging.debug('changing img')
+    return "new_img.txt"
+
+def save_img(img):
+    logging.debug('saving img')
+    
+
+def main():
+    logging.debug('Start of the program.')
+    fname = get_filename()
+    img = get_img(fname)
+    output = change_img(img)
+    save_img(output)
+    logging.debug('End of program.')
+
+if __name__ == "__main__":
+    main()
