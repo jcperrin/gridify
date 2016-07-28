d612bcfce48d622bd0547cbe3b7693e9714fd938
Jean-Christophe Perrin <jc.m.perrin@gmail.com>
2016-07-29 00:14:51 +0530

    Matrix of Panes created
    First steps taken towards breaking down photo into individual panes.
    The panes do not overlap currently, and there has been minimal bug
    checking. However, I was able to set up a 2D array to hold all of the
    cropped panes.
    
    TODO: confirm the panes are cropping correctly
    TODO: sample overlapping planes

--- grid_it.py
+++ grid_it.py
@@ -9,6 +9,10 @@ import sys
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 #logging.disable(logging.CRITICAL)
 
+#"Constants"
+NUM_COLS = 2
+NUM_ROWS = 2
+
 #this function prompts the user for a valid file name
 #it loops until a valid name is entered
 #
@@ -23,12 +27,31 @@ def get_filename():
             print('File not found.\n')
     return name
 
-def get_img(img):
+#This funciton opens and sets up the image for manipulation
+def get_img(fname):
     logging.debug('getting img')
-    return "img.png"
+    return Image.open(fname)
 
+#this function does the actual image manipulation
+#The change intended is to create a grid of spread blocks
+#ultimately, the blocks will be sampled from overlapping frames
 def change_img(img):
     logging.debug('changing img')
+    logging.debug( img.size)
+    img_width, img_height = img.size
+    logging.debug(img_width)
+    logging.debug(img_height)
+
+    pane_width = img_width / NUM_COLS
+    pane_height = img_height / NUM_ROWS
+    panes = [[0 for x in range(NUM_COLS)] for y in range(NUM_ROWS)]
+    for x in range(NUM_COLS):
+        for y in range(NUM_ROWS):
+            ulx = x * pane_width
+            uly = y * pane_height
+            pane = (ulx, uly, ulx + pane_width, uly + pane_height)
+            panes[x][y] = img.crop(pane)
+     
     return "new_img.txt"
 
 def save_img(img):
@@ -37,7 +60,8 @@ def save_img(img):
 
 def main():
     logging.debug('Start of the program.')
-    fname = get_filename()
+   # fname = get_filename()
+    fname = 'face.jpg'
     img = get_img(fname)
     output = change_img(img)
     save_img(output)
@@ -45,3 +69,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
---
+++ readme.txt
@@ -0,0 +1,23 @@
+Hi
+
+This program is an introduction to photo manipulation in python.
+It's inspired by a photo that I saw on the internet today
+describing how computer vision works. In it, the author
+uses a photo of his son, broken into smaller panes and spread out
+in order to illustrate the idea of a computer looking at sub-secitons
+of a photo in order to apply machine learning to it.
+
+The frames were of overlapping parts of the source image, but
+were spread out such that the composite had the effect of looking
+through a kaleidescope or compound eyes.
+
+This program is meant to take any photo, and to appply the same effect
+to it. It will eventually prompt the user both for the type of grid
+and/or the columns and rows but for now I'm just getting functionality
+up and running for a set number of rows.
+
+You can find the original photo here:
+https://d262ilb51hltx0.cloudfront.net/max/800/1*xS7EugfgQHk68iph7GHpQg.png
+
+@author JC Perrin
+jc.m.perrin@gmail.com
